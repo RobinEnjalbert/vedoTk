b4fe2d738d5de89ce329c355834d312ccdffe529
robin <robin.enjalbrt@gmail.com>
2023-03-14 17:13:02 +0100

    Enable inheritance from VisiblePoints.

--- src/visible_points.py
+++ src/visible_points.py
@@ -12,7 +12,7 @@ TVisiblePoints = TypeVar('TVisiblePoints', bound='VisiblePoints')
 class VisiblePoints:
 
     def __init__(self,
-                 mesh: Union[Mesh, List[ndarray, ndarray]]):
+                 mesh: Union[Mesh, List[ndarray]]):
         """
         Extract the visible nodes of a mesh depending on the camera parameters.
         If using in a plotter instance, use vedo.visible_points instead.
@@ -22,15 +22,15 @@ class VisiblePoints:
         """
 
         # Mesh information
-        self.__mesh: Mesh = mesh if type(mesh) == Mesh else Mesh(mesh)
-        self.__visible_points: ndarray = array([])
-        self.__indices_visible_points: List[int] = []
+        self._mesh: Mesh = mesh if type(mesh) == Mesh else Mesh(mesh)
+        self._visible_points: ndarray = array([])
+        self._indices_visible_points: List[int] = []
 
         # Camera information
-        self.__camera_position: List[float] = [0., 0., 1.]
-        self.__focal_point: List[float] = [0., 0., 0.]
-        self.__view_angle: float = 30.
-        self.__distance_threshold = float('Inf')
+        self._camera_position: List[float] = [0., 0., 1.]
+        self._focal_point: List[float] = [0., 0., 0.]
+        self._view_angle: float = 30.
+        self._distance_threshold = float('Inf')
 
     def set_camera(self,
                    camera_position: Optional[List[float]] = None,
@@ -46,10 +46,10 @@ class VisiblePoints:
         :param distance_threshold: Max distance to the camera for a point to be detected.
         """
 
-        self.__camera_position = self.__camera_position if camera_position is None else camera_position
-        self.__focal_point = self.__focal_point if focal_point is None else focal_point
-        self.__view_angle = self.__view_angle if view_angle is None else view_angle
-        self.__distance_threshold = self.__distance_threshold if distance_threshold is None else distance_threshold
+        self._camera_position = self._camera_position if camera_position is None else camera_position
+        self._focal_point = self._focal_point if focal_point is None else focal_point
+        self._view_angle = self._view_angle if view_angle is None else view_angle
+        self._distance_threshold = self._distance_threshold if distance_threshold is None else distance_threshold
         return self
 
     def extract(self,
@@ -61,19 +61,19 @@ class VisiblePoints:
         """
 
         # Launch the extractor in a new thread
-        t = Thread(target=self.__extract, args=(positions,))
+        t = Thread(target=self._extract, args=(positions,))
         t.start()
         t.join()
-        return self.__visible_points, self.__indices_visible_points
+        return self._visible_points, self._indices_visible_points
 
-    def __extract(self, positions):
+    def _extract(self, positions):
 
         # Create the mesh
-        mesh = self.__mesh.clone() if positions is None else Mesh([positions, self.__mesh.cells()])
+        mesh = self._mesh.clone() if positions is None else Mesh([positions, self._mesh.cells()])
         mesh.compute_normals()
 
         # Format the camera with position and focal point, extract visible nodes
-        cam = dict(pos=self.__camera_position, focalPoint=self.__focal_point, viewAngle=self.__view_angle)
+        cam = dict(pos=self._camera_position, focalPoint=self._focal_point, viewAngle=self._view_angle)
         plt = show(mesh, new=True, camera=cam, offscreen=True)
         visible_pcd = visible_points(mesh)
         plt.close()
@@ -83,12 +83,12 @@ class VisiblePoints:
         list_positions = np_round(mesh.points().copy(), 6).tolist()
         indices = []
         for point in visible_pcd.points():
-            if norm(point - self.__camera_position) < self.__distance_threshold:
+            if norm(point - self._camera_position) < self._distance_threshold:
                 filtered_visible_points.append(point)
                 position = np_round(point, 6).tolist()
                 try:
                     indices.append(list_positions.index(position))
                 except ValueError:
                     pass
-        self.__visible_points = array(filtered_visible_points)
-        self.__indices_visible_points = indices
+        self._visible_points = array(filtered_visible_points)
+        self._indices_visible_points = indices
